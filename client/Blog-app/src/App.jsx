a56a8152d594dfe8c8dbbaa27495dcdca8835643
harshalkodgirwar <harshalkod2003@gmail.com>
2023-05-17 00:47:43 +0530

    initial commit

---
+++ client/Blog-app/src/App.jsx
@@ -0,0 +1,83 @@
+import { Children, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom";
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Single from "./pages/Single"
+import Write from "./pages/Write"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import "./style.scss"
+
+const Layout = () => {
+  return(
+    <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+    </>
+  );
+};
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      },
+
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  
+]);
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="app">
+        <div className="container">
+
+      
+        <RouterProvider router={router}/>
+      
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+
+export default App
