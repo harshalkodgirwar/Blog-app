a8339b63758977f5352f22eebd3e472cb7a574bb
harshalkodgirwar <harshalkod2003@gmail.com>
2023-05-17 17:01:16 +0530

    commit

--- client/Blog-app/src/components/Footer.jsx
+++ client/Blog-app/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import logo from '../images/logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={logo} alt="" />
+      <span>Made with love and <b>React.js</b></span>
+    </footer>
   )
 }
 
--- client/Blog-app/src/components/Navbar.jsx
+++ client/Blog-app/src/components/Navbar.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import logo from '../images/logo.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='container'>
-    <div className="logo">logo</div>
-      <div className="links">links</div>
+    <div className="logo">
+    <img src={logo} alt="" />
+    </div>
+
+    <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>John</span>
+          <span>logout</span>
+          <span className="write">
+            <Link className="link" to='/write'>Write</Link>
+          </span>
+          </div>
     
       </div>
     </div>
